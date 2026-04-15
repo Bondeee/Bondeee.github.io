ce3cf972917ec62f16911063d34fb56aea5120f5
Bondesa diego <diego.bondesan08@gmail.com>
2026-04-15 13:46:38 +0200

    correzione dell'esercizio sulla registrazione e inizio esercizio sul quiz

---
+++ progetti/JsQuiz/script.js
@@ -0,0 +1,68 @@
+let mes = document.getElementById("mese");
+
+
+
+let punti = 0;
+
+let mesi = ["GEN","FEB","MAR","APR","MAG","GIU","LUG","AGO","SET","OTT","NOV","DIC"];
+
+function riempiMesi() {
+    let html = "";
+    for (let i = 0; i <= 11; i++)
+        html += "<option value=" + mesi[i] + ">" + mesi[i] + "</option>";
+    mes.innerHTML = html;
+}
+
+window.onload = function () { riempiMesi() }
+
+let risposte2 = ["1","2","4","256"];
+function riempiR2() {
+    let html = "";
+    for (let i = 0; i <= 3; i++)
+        html += "<option value=" + risposte2[i] + ">" + risposte2[i] + "</option>";
+    r2.innerHTML = html;
+}
+window.onload = function () { riempiR2() }
+
+let rispCorrette = ["SQL", "2", "Dinamiche", true, false, true, "Byte"];
+let rispDate = [];
+
+function ElaboraRisposte() {
+    punti = 0;
+
+    //prima risposta
+    rispDate[0] = document.getElementById("r1").value;
+
+    //seconda risposta
+    rispDate[1] = document.getElementById("r2").value;
+
+    //terza risposta
+    let opzioni = document.getElementsByName("tipoLibrerie");
+    for (let i = 0; i < opzioni.length; i++)
+        if (opzioni[i].checked) 
+            rispDate[2] = opzioni[i].value;
+    
+    //quarta risposta
+    rispDate[3] = document.getElementById("void").checked;
+    rispDate[4] = document.getElementById("integer").checked;
+    rispDate[5] = document.getElementById("char").checked;
+
+    //quinta risposta
+    rispDate[6] = document.getElementById("r5").value;
+
+    // Confronto
+    let indiciSingoli = [0, 1, 2, 6]; 
+    for (let i of indiciSingoli) 
+        if (rispDate[i] === rispCorrette[i]) 
+            punti++;
+    
+
+    if (rispDate[3] === rispCorrette[3] && 
+        rispDate[4] === rispCorrette[4] && 
+        rispDate[5] === rispCorrette[5]) {
+        punti++; 
+    }
+
+    
+}
+    
--- progetti/Registrazione Utente/script.js
+++ progetti/Registrazione Utente/script.js
@@ -1,42 +1,39 @@
 let anni = document.getElementById("anni");
-let nome = document.getElementById("nome");
-let cognome = document.getElementById("cognome");
-let cf = document.getElementById("CF").value;
 let termini = document.getElementById("Termini");
 let annoCorrente = new Date().getFullYear();
 
-function riempiAnni() 
-{
+function riempiAnni() {
     let html = "";
-    for(let i = 1900; i <= annoCorrente; i++)
-        html +="<option value=" + i + ">" + i + "</option>";
+    for (let i = 1900; i <= annoCorrente; i++)
+        html += "<option value=" + i + ">" + i + "</option>";
     anni.innerHTML = html;
 }
 
-window.onload = function() { riempiAnni() }
+window.onload = function () { riempiAnni() }
 
-function CompletamentoRegistrazione(){
-    if(nome.value == "" || cognome.value == "" || cf == "")
-    { alert("devi inserire il nome il cognome \n e il codice fiscale prima della registrazione"); return;}
-    
-    if(termini.checked == false) { alert("Devi prima accettare i termini di servizio"); return; }
+function CompletamentoRegistrazione() {
+
+    let nome = document.getElementById("nome").value;
+    let cognome = document.getElementById("cognome").value;
+    let cf = document.getElementById("CF").value;
+
+    if (nome === "" || cognome === "" || cf === "") { alert("devi inserire il nome il cognome \n e il codice fiscale prima della registrazione"); return; }
+
+    if (termini.checked === false) { alert("Devi prima accettare i termini di servizio"); return; }
 
     let check = true;
-    if(cf > 16 )
-    {
-        for(let i = 0; i < cf.length; i++)
-        {
-            if(i < 5 )
-            {
-                if(cf[i] >= 'z' && cf[i] <= 'a' || cf[i] > 'Z' && cf[i] <= 'A')
+    if (cf > 16) {
+        for (let i = 0; i < cf.length; i++) {
+            if (i < 5) {
+                if (cf[i] >= 'z' && cf[i] <= 'a' || cf[i] > 'Z' && cf[i] <= 'A')
                     check = false;
             }
 
         }
     }
 
-    let message= "";
-    message +="Gentile sig./sig.ra " + cognome.value + " " + nome.value + " \n la ringraziamo per la registrazione";
+    let message = "";
+    message += "Gentile sig./sig.ra " + cognome + " " + nome + " \n la ringraziamo per la registrazione";
 
     alert(message);
 }
